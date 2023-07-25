885e62f7e5242743431132b98dbe2c2e32024b95
belfarz <115107231+belfarz@users.noreply.github.com>
2023-07-25 06:26:04 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Navbar from "./components/Navbar"
 import Service from "./components/Service";
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Footer from "./components/Footer";
@@ -14,14 +14,14 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <BrowserRouter>
+      
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/gym' element={<Home />} />
             <Route path='/Service' element={<Service />} />
             <Route path='/About' element={<About />} />
           </Routes>
-          </BrowserRouter>
+          
       <Footer />
 
 
